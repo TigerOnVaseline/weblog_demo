adc49b2ff7fae555f9ed86ce296e6e39d17a7150
TigerOnVaseline <>
2020-07-22 21:58:48 +0200

    Initial commit

--- 05_top_ten_ip_barchart.sh
+++ 05_top_ten_ip_barchart.sh
@@ -8,7 +8,7 @@
 
 # Linted with shellcheck: https://github.com/koalaman/shellcheck
 
-# On MacOS, install bash >= 4.0 with Homebrew. 
+# Requires bash >= 4.0  
 # YMMV.
 
 # Read arguments and count into arrays to prevent them getting mangled
@@ -27,7 +27,6 @@ readonly MONTHS=( _ Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec )
 readonly BAR_GLYPH='#'
 readonly ACCESS_LOG="access.log"
 
-
 get_barchart_chars() {
     local num_chars="$1"
     local bar_char="$2"
@@ -43,8 +42,7 @@ get_barchart_chars() {
     printf "%s" "${char_list}"
 }
 
-main()
-{
+main(){
     local required_command
     local year
     local month
@@ -76,65 +74,60 @@ main()
     echo "Top 10 IPs bar chart per month"
     for ((year=START_YEAR; year<=END_YEAR; year++)) 
     { 
-    for ((month=START_MONTH; month<=END_MONTH; month++))
-      {
-        echo "${month[${START_MONTH}]}"
+        for ((month=START_MONTH; month<=END_MONTH; month++))
+        {
+            echo "${month[${START_MONTH}]}"
+            top_ten_ip_addresses=()
+            top_ten_ip_addresses_count=()
+            while read -r ip_address ip_address_count;
+            do
+                top_ten_ip_addresses+=("${ip_address_count}")
+                top_ten_ip_addresses_count+=("${ip_address}")
+            done < <( \
+                awk -v month_year="${MONTHS[${month}]}/${year}" \
+                '$4 ~ month_year {print $1}' ${ACCESS_LOG} | \
+                sort | \
+                uniq -c | \
+                sort -rn | \
+                head -n 10 )
 
-        top_ten_ip_addresses=()
-        top_ten_ip_addresses_count=()
-        while read -r ip_address ip_address_count;
-        do
-          top_ten_ip_addresses+=("${ip_address_count}")
-          top_ten_ip_addresses_count+=("${ip_address}")
-        done < <( 
-        awk -v month_year="${MONTHS[${month}]}/${year}" \
-        '$4 ~ month_year {print $1}' ${ACCESS_LOG} | \
-        sort | \
-        uniq -c | \
-        sort -rn | \
-        head -n 10 )
+                # Let's not waste time, bail out now if there are no hits for
+                # that period in the logs.
+                if [[ ${#top_ten_ip_addresses[@]} -le 1 ]]; then
+                    echo "Zero matches in ${ACCESS_LOG} for ${MONTHS[${month}]}/${year}"
+                    break
+                fi
 
-        # Let's not waste time, bail out now if there are no hits for
-        # that period in the logs.
-        if [[ ${#top_ten_ip_addresses[@]} -le 1 ]]; then
-            echo "Zero matches in ${ACCESS_LOG} for ${MONTHS[${month}]}/${year}"
-            break
-        fi
+                echo "${MONTHS[${month}]} ${year}"
 
-        echo "${MONTHS[${month}]} ${year}"
+                # Yes, this whole rigmarole again. 
+                # We'll need it later for the bar chart calculation.
+                total_hits_per_period=$(
+                awk -v month_year="${MONTHS[${month}]}/${year}" \
+                '$4 ~ month_year {print $1}' ${ACCESS_LOG} | wc -l)
 
-        # Yes, this whole rigmarole again. 
-        # We'll need it later for the bar chart calculation.
-        total_hits_per_period=$(
-        awk -v month_year="${MONTHS[${month}]}/${year}" \
-        '$4 ~ month_year {print $1}' ${ACCESS_LOG} | wc -l)
-        
-        # Determine how many significant figures in first IP address count,
-        # this will be needed for the printf() in the for loop later
-        column_length=${#top_ten_ip_addresses_count[1]}
-    
-        for top_list_item in "${!top_ten_ip_addresses[@]}"; do
+                # Determine how many significant figures in first IP address count,
+                # this will be needed for the printf() in the for loop later
+                column_length=${#top_ten_ip_addresses_count[1]}
 
-        count=${top_ten_ip_addresses_count[${top_list_item}]}
-        ip_address=${top_ten_ip_addresses[${top_list_item}]}
-
-        # bash arithmetic only supports integers, so use awk instead
-        barchart_percentage=$(awk \
-        -vtop="${total_hits_per_period}" \
-        -vcount="${count}" \
-        'BEGIN{printf("%.f\n",(count/top)*100)}')
-
-        barchart=$(get_barchart_chars "${barchart_percentage}" "${BAR_GLYPH}")
-
-        # Print a pretty table with the final scores
-        printf "%${column_length}d\t%s\t(%s%%)\t%s\n" \
-        "${count}"  \
-        "${ip_address}" \
-        "${barchart_percentage}" \
-        "${barchart}" 
-        done
-      }
-    }
+                for top_list_item in "${!top_ten_ip_addresses[@]}"; do
+                    count=${top_ten_ip_addresses_count[${top_list_item}]}
+                    ip_address=${top_ten_ip_addresses[${top_list_item}]}
+                    # bash arithmetic only supports integers, so use awk instead
+                    barchart_percentage=$(awk \
+                    -vtop="${total_hits_per_period}" \
+                    -vcount="${count}" \
+                    'BEGIN{printf("%.f\n",(count/top)*100)}')
+                    barchart=$(get_barchart_chars "${barchart_percentage}" "${BAR_GLYPH}")
+                    # Print a pretty table with the final scores
+                    printf "%${column_length}d\t%s\t(%s%%)\t%s\n" \
+                    "${count}"  \
+                    "${ip_address}" \
+                    "${barchart_percentage}" \
+                    "${barchart}" 
+                done
+            }
+        }
 }
 
 main "${ARGV[@]}" 
